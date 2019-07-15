b05169cebc31d89feeea42b25934df1a6ea4a2fa
alecsimone <alecsimone@gmail.com>
2019-07-15 13:35:42 -0600

    homepage working

---
+++ Frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+    render() {
+        return <p>Hey hey you you I don't like your girlfriend!</p>
+    }
+}
+
+
+export default Home;
