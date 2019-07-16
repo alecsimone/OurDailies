ff3ab39f8c41fa438b042d4a7dee9aafc4a708e7
alecsimone <alecsimone@gmail.com>
2019-07-15 22:16:45 -0600

    primitive styling

--- Frontend/components/Nav.js
+++ Frontend/components/Nav.js
@@ -1,14 +1,26 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const NavBar = styled.div`
+    font-size: 2rem;
+    a {
+        margin: 0 .5rem;
+        color: ${props => props.theme.white};
+        :visited {
+            color: ${props => props.theme.white};
+        }
+    }
+`;
 
 const Nav = () => (
-    <div>
+    <NavBar>
         <Link href="/">
             <a>Home</a>
         </Link>
         <Link href="/second">
             <a>Second</a>
         </Link>
-    </div>
+    </NavBar>
 )
 
 export default Nav;
--- Frontend/components/Page.js
+++ Frontend/components/Page.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+const theme = {
+    black: 'hsl(200, 17%, 7%)', 
+    white: 'hsl(30, 5%, 90%)',
+    blue: 'hsl(210, 100%, 67%)',
+    gold: 'hsl(42, 61%, 93%)',
+    darkBlue: 'hsl(210, 100%, 33%)',
+    darkGrey: 'hsl(30, 10%, 33%)',
+    lightGrey: 'hsl(30, 10%, 67%)',
+}
+
+injectGlobal`
+    html {
+        background: ${theme.black};
+        font-family: sans-serif;
+        font-size: 12px;
+        box-sizing: border-box;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem;
+        line-height: 2;
+    }
+    a {
+        text-decoration: none;
+    }
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
-                <Meta />
-                <Header />
-                {this.props.children}
-            </div>
+            <ThemeProvider theme={theme}>
+                <div>
+                    <Meta />
+                    <Header />
+                    {this.props.children}
+                </div>
+            </ThemeProvider>
         );
     }
 }
