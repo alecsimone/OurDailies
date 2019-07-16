2d85aa50780b5e4bf488e5842e4debed5cd2fe26
alecsimone <alecsimone@gmail.com>
2019-07-16 01:53:49 -0600

    load css before render

--- Frontend/components/Header.js
+++ Frontend/components/Header.js
@@ -1,4 +1,16 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 
 const Header = () => (
     <div>
