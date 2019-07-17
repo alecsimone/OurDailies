8b72097ff338f7f6ff9702593a5177b8f7555a24
alecsimone <alecsimone@gmail.com>
2019-07-16 22:51:59 -0600

    GraphQL setup finished

--- Backend/src/resolvers/Mutation.js
+++ Backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+    async createThing(parent, args, ctx, info) {
+        const thing = await ctx.db.mutation.createThing({
+            data: {...args}
+        }, info);
+
+        return thing;
+    }
+};
 
 module.exports = Mutations;
--- Backend/src/resolvers/Query.js
+++ Backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+    async things(parent, args, ctx, info) {
+        const things = ctx.db.query.things();
+        return things;
+    }
+};
 
 module.exports = Query;
