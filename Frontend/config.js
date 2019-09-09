f8c4c723a766d28d232ebe06e217e28c1849b839
alecsimone <alecsimone@gmail.com>
2019-09-09 10:10:29 -0600

    changed endpoint to custom domain

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,7 +1,7 @@
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://ourdailies-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `http://playground.ourdailies.org/`;
 export const endpointNoHTTP = `localhost:4444`;
-export const prodEndpointNoHTTP = `ourdailies-yoga-prod.herokuapp.com`;
+export const prodEndpointNoHTTP = `playground.ourdailies.org`;
 export const home = 'http://localhost:7777';
 export const homeNoHTTP = 'localhost:7777';
 export const perPage = 4;
