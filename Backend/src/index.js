a38f9526540e33359dc0d70db1b17123ca15eb72
alecsimone <alecsimone@gmail.com>
2019-09-09 13:30:06 -0600

    trying a different pattern for cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -37,7 +37,11 @@ server.start(
    {
       cors: {
          credentials: true,
-         origin: ['http://localhost:*', 'https://www.twitch.tv/popout/ourdailies/chat?popout=', 'http*://ourdailies.org']
+         origin: [
+            'http://localhost:*',
+            'https://www.twitch.tv/popout/ourdailies/chat?popout=',
+            'http*://*ourdailies.org'
+         ]
       }
    },
    serverDetails => {
